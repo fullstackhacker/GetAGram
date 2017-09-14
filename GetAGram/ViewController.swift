adfc0df86f06ada904e6fa4f98ee0eeea8ec739d
kapadiamush <kapadiamush@gmail.com>
2017-09-13 18:17:46 -0700

    Initial Commit

---
+++ GetAGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetAGram
+//
+//  Created by Mushaheed Kapadia on 9/13/17.
+//  Copyright © 2017 twins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
